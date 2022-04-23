e5d65c12fb7b8c987dfdb591aa455006a6bae282
lhonRafaat <lhonRafaat@gmail.com>
2022-04-23 17:39:37 +0300

    checked for user profile whenever a msg is sent  to see if origin p exists, else adds to db

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ import env from "dotenv";
 import { Client, Intents } from "discord.js";
 
 import memberRoutes from "./routes/memberRoutes.js";
-import { findOne } from "./services/memberService.js";
+import { createMember, findOne } from "./services/memberService.js";
+import axios from "axios";
 
 env.config();
 const app = express();
@@ -25,11 +26,50 @@ client.on("ready", () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
-client.on("messageCreate", (msg) => {
+client.on("messageCreate", async (msg) => {
+  //we dont want messages from the bot
+
   if (msg.author.bot) return;
-  console.log(msg.client.user.id);
-  const user = findOne(msg.client.user.id);
-  if(!user)
+
+  // check to see if we already have this user in db
+  const user = await findOne(msg.author.id);
+  console.log(user);
+
+  // if we did not have the user , then we get to work
+  if (!user) {
+    // these console logs are only for debugging , dont worry
+
+    console.log("here");
+
+    //making a http request to gametools api to find the user profile, but idk what to do about the game and platform , beacause they are required
+    // meaning a game and platform is always chosen which ofcourse is a issue.
+
+    const member = axios
+      .get(
+        `https://api.gametools.network/bfv/all/?format_values=false&name=${msg.member.nickname}&lang=en-us&platform=pc&`
+      )
+      .then((returnedMember) => {
+        console.log(returnedMember.data);
+
+        //check if the user's profile exists
+        if (returnedMember?.data?.id) {
+          console.log("here2");
+
+          //if the user's profile exists , then we create a new member in the db
+
+          createMember(
+            msg.author.id,
+            returnedMember.data.id,
+            "pc",
+            returnedMember.data.platoons
+              .map((el) => el.id)
+              .includes("fbc7c5ab-c125-41f9-be8c-f367c03b2551"),
+            msg.author.username
+          );
+        }
+      });
+  }
+  // in case you are very bored
   if (msg.content.toLowerCase() === "ping") {
     msg.reply("pong");
   }

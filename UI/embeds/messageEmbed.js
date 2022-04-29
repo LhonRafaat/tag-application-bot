5488994b06d8b057cd5008a08da08c8f68aedfe0
lhonRafaat <lhonRafaat@gmail.com>
2022-04-29 18:49:16 +0300

    changed embed messages content, and made the name dynamic

--- UI/embeds/messageEmbed.js
+++ UI/embeds/messageEmbed.js
@@ -1,32 +1,39 @@
 // at the top of your file
 import { MessageEmbed } from "discord.js";
 
+const description = `
+   iDF is a community of players who enjoy playing games together.
+   We are a group of players who enjoy playing together and have a lot of fun.
+   to join us, please click the button below.
+   to vote for a player to join us, please click the button below.
+   `;
+
 // inside a command, event listener, etc.
 export const tagEmbed = new MessageEmbed()
   .setColor("#0099ff")
-  .setTitle("Vote for a user")
+  .setTitle("iDF Tag Appplication")
   // .setURL("https://discord.js.org/")
   .setAuthor({
-    name: "Me",
+    name: "iDF",
     iconURL:
       "https://eaassets-a.akamaihd.net/battlelog/prod/emblem/392/590/320/2955055690685760912.png?v=1537200736",
     url: "https://discord.js.org",
   })
-  .setDescription("Vote for a desired user")
-  .setThumbnail(
-    "https://eaassets-a.akamaihd.net/battlelog/prod/emblem/392/590/320/2955055690685760912.png?v=1537200736"
-  )
-  .addFields(
-    { name: "Regular field title", value: "Some value here" },
-    { name: "\u200B", value: "\u200B" },
-    { name: "Inline field title", value: "Some value here", inline: true },
-    { name: "Inline field title", value: "Some value here", inline: true }
-  )
-  .setImage(
-    "https://eaassets-a.akamaihd.net/battlelog/prod/emblem/392/590/320/2955055690685760912.png?v=1537200736"
-  )
-  .setFooter({
-    text: "Some footer text here",
-    iconURL:
-      "https://eaassets-a.akamaihd.net/battlelog/prod/emblem/392/590/320/2955055690685760912.png?v=1537200736",
-  });
+  .setDescription(description);
+// .setThumbnail(
+//   "https://eaassets-a.akamaihd.net/battlelog/prod/emblem/392/590/320/2955055690685760912.png?v=1537200736"
+// )
+// .addFields(
+//   { name: "Regular field title", value: "Some value here" },
+//   { name: "\u200B", value: "\u200B" },
+//   { name: "Inline field title", value: "Some value here", inline: true },
+//   { name: "Inline field title", value: "Some value here", inline: true }
+// )
+// .setImage(
+//   "https://eaassets-a.akamaihd.net/battlelog/prod/emblem/392/590/320/2955055690685760912.png?v=1537200736"
+// )
+// .setFooter({
+//   text: "Some footer text here",
+//   iconURL:
+//     "https://eaassets-a.akamaihd.net/battlelog/prod/emblem/392/590/320/2955055690685760912.png?v=1537200736",
+// });
--- UI/embeds/voteEmbed.js
+++ UI/embeds/voteEmbed.js
@@ -7,30 +7,36 @@ import {
 } from "../../emojies/emojies.js";
 
 // inside a command, event listener, etc.
-export const voteEmbed = new MessageEmbed()
-  .setColor("#0099ff")
-  .setTitle("Vote for a user")
-  .setURL("https://discord.js.org/")
-  .setAuthor({
-    name: "iDF",
-    iconURL:
-      "https://eaassets-a.akamaihd.net/battlelog/prod/emblem/392/590/320/2955055690685760912.png?v=1537200736",
-    url: "https://discord.js.org",
-  })
-  .setDescription("Vote for a desired user")
-  .setThumbnail(
-    "https://eaassets-a.akamaihd.net/battlelog/prod/emblem/392/590/320/2955055690685760912.png?v=1537200736"
-  )
-  .addFields(
-    { name: "Skill", value: SKILL_EMOJI, inline: true },
-    { name: "Contribution", value: CONTRIBUTION_EMOJI, inline: true },
-    { name: "Personality", value: PERSONALITY_EMOJI, inline: true }
-  )
-  .setImage(
-    "https://eaassets-a.akamaihd.net/battlelog/prod/emblem/392/590/320/2955055690685760912.png?v=1537200736"
-  )
-  .setFooter({
-    text: "Some footer text here",
-    iconURL:
-      "https://eaassets-a.akamaihd.net/battlelog/prod/emblem/392/590/320/2955055690685760912.png?v=1537200736",
-  });
+export const getVoteEmbed = (name) => {
+  const voteEmbed = new MessageEmbed()
+    .setColor("#0099ff")
+    .setTitle(`Vote for a ${name}`)
+    .setURL("https://discord.js.org/")
+    .setAuthor({
+      name: "iDF",
+      iconURL:
+        "https://eaassets-a.akamaihd.net/battlelog/prod/emblem/392/590/320/2955055690685760912.png?v=1537200736",
+      url: "https://discord.js.org",
+    })
+    .setDescription(
+      "to vote for, please react to each reaction below accordingly"
+    )
+    .setThumbnail(
+      "https://eaassets-a.akamaihd.net/battlelog/prod/emblem/392/590/320/2955055690685760912.png?v=1537200736"
+    )
+    .addFields(
+      { name: "Skill", value: SKILL_EMOJI, inline: true },
+      { name: "Contribution", value: CONTRIBUTION_EMOJI, inline: true },
+      { name: "Personality", value: PERSONALITY_EMOJI, inline: true }
+    );
+  // .setImage(
+  //   "https://eaassets-a.akamaihd.net/battlelog/prod/emblem/392/590/320/2955055690685760912.png?v=1537200736"
+  // )
+  // .setFooter({
+  //   text: "Some footer text here",
+  //   iconURL:
+  //     "https://eaassets-a.akamaihd.net/battlelog/prod/emblem/392/590/320/2955055690685760912.png?v=1537200736",
+  // });
+
+  return voteEmbed;
+};

92363ce4ea667e7c1800f4d96e538b3f7cea0299
lhonRafaat <lhontaekwondo@gmail.com>
2022-05-02 20:16:37 +0300

    implemented linking another account

---
+++ UI/linkAnotherAccountModal.js
@@ -0,0 +1,24 @@
+import { Modal, TextInputComponent } from "discord-modals";
+
+export const linkAnotherAccountModal = () => {
+  return new Modal() // We create a Modal
+    .setCustomId("linkAnotherAccount")
+    .setTitle("Register for iDF tag application")
+    .addComponents(
+      new TextInputComponent() // We create a Text Input Component
+        .setCustomId("gameNameVal")
+        .setLabel("Enter your game name (origin)")
+        .setStyle("SHORT") //IMPORTANT: Text Input Component Style can be 'SHORT' or 'LONG'
+        .setRequired(true), // If it's required or not
+      new TextInputComponent() // We create a Text Input Component
+        .setCustomId("platformVal")
+        .setLabel("Enter your platform (pc,xbox,ps)")
+        .setStyle("SHORT") //IMPORTANT: Text Input Component Style can be 'SHORT' or 'LONG'
+        .setRequired(true), // If it's required or not
+      new TextInputComponent() // We create a Text Input Component
+        .setCustomId("gameVal")
+        .setLabel("Enter your game (bf1,bf4,bfv ..)")
+        .setStyle("SHORT") //IMPORTANT: Text Input Component Style can be 'SHORT' or 'LONG'
+        .setRequired(true) // If it's required or not
+    );
+};

5b77ca963aa5df5fac433188490f5fba4b9269a3
lhonRafaat <lhontaekwondo@gmail.com>
2022-05-04 00:19:53 +0300

    fill bar progress

--- UI/userPlate.js
+++ UI/userPlate.js
@@ -23,10 +23,25 @@ export const getPlate = async (name, discordId, userAvatar, secondName) => {
 
   const avatar = await Canvas.loadImage(userAvatar);
   const avatarFrame = await Canvas.loadImage("./assets/images/frame.png");
+  const numBox = await Canvas.loadImage("./assets/images/num-box.png");
+  const fillBar = await Canvas.loadImage("./assets/images/fill-bar.png");
+  const sparks = await Canvas.loadImage("./assets/images/sparks.png");
 
   //the avatar and frame have 15 pixels of space differencem and 42 in high and width,, this is an estimate
   context.drawImage(avatar, 40, 40, 208, 208);
   context.drawImage(avatarFrame, 25, 25, 250, 250);
+  context.drawImage(numBox, canvas.width / 2 + 150, 309, 55, 55);
+  context.drawImage(numBox, canvas.width / 2 + 150, 379, 55, 55);
+  context.drawImage(numBox, canvas.width / 2 + 150, 449, 55, 55);
+  // calculating the fill bar width
+  const barLength = canvas.width / 2 / requiredPoints;
+
+  context.globalCompositeOperation = "lighter";
+  // the category name characters are not even , so I need to add numbers to make the fill bar even
+  context.drawImage(fillBar, 100, 260, 5 * barLength + 50, 130);
+  context.drawImage(fillBar, 330, 330, 20 * barLength - 180, 130);
+  context.drawImage(fillBar, 150, 400, 20 * barLength, 130);
+
   context.strokeRect(0, 0, canvas.width, canvas.height);
 
   // Select the font size and type from one of the natively available fonts
@@ -55,10 +70,14 @@ export const getPlate = async (name, discordId, userAvatar, secondName) => {
   context.fillRect(150, 330, canvas.width / 2, 10);
   context.globalAlpha = 1.0;
 
-  context.fillStyle = grd;
-  context.fillRect(canvas.width / 2 + 190, 320, 42, 42);
+  // context.fillStyle = grd;
+  // context.fillRect(canvas.width / 2 + 190, 320, 42, 42);
 
-  context.fillText(user.skills, canvas.width / 2 + 200, 350);
+  context.font = "600 30px sans-serif";
+
+  context.fillText("12", canvas.width / 2 + 158, 350);
+  context.fillText("20", canvas.width / 2 + 158, 420);
+  context.fillText("10", canvas.width / 2 + 158, 490);
 
   // Use the helpful Attachment class structure to process the file for you
   const attachment = new MessageAttachment(

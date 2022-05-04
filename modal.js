4d3c3d219fbad17a36de0262d141ec15fe078102
lhonRafaat <lhontaekwondo@gmail.com>
2022-05-04 23:11:55 +0300

    forgot to add this, same as prev commit

--- modal.js
+++ modal.js
@@ -20,6 +20,37 @@ export const getModal = (client) => {
   let searchedName;
 
   client.on("interactionCreate", async (interaction) => {
+    if (interaction.commandName === "getstatus") {
+      const mentionedUser = interaction.options.getUser("username");
+
+      const discordUser = await findOne(mentionedUser.id);
+
+      if (!discordUser) {
+        return interaction.reply({
+          content: "User not found",
+          ephemeral: true,
+        });
+      }
+
+      //TODO : how can I get the user avatar from discord?
+
+      // I could have just passed the whole user object here instead of passing discord id and fetching it again
+      // in the plate, but idk why I did that ...
+      const attachment = await getPlate(
+        discordUser.userNames[0],
+        discordUser.discordId,
+        discordUser.avatar,
+        discordUser.userNames[1] ? discordUser.userNames[1] : undefined
+      );
+      //TODO: send a error message when user doesnt exist
+
+      // big problem here, if ephemeral is true, we cannot react to the messag
+      return await interaction.reply({
+        ephemeral: true,
+        files: [attachment],
+      });
+    }
+
     if (interaction.commandName === "vote") {
       const mentionedUser = interaction.options.getUser("username");
 

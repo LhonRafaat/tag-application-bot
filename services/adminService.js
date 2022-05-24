510120cdfdda63a12c18d0f0954f789f9d59c6aa
lhonRafaat <lhontaekwondo@gmail.com>
2022-05-24 14:11:33 +0300

    checked confirm password at register admin

--- services/adminService.js
+++ services/adminService.js
@@ -4,9 +4,15 @@ import jwt from "jsonwebtoken";
 import bcrypt from "bcrypt";
 
 export const signup = async (userData) => {
+  if (!userData.email || !userData.password)
+    throw new Error("Username and password are required");
+
+  if (userData.password !== userData.confirmPassword)
+    throw new Error("Passwords do not match");
   const admin = await Admin.create({
     ...userData,
     password: await bcrypt.hash(userData.password, 10),
+    confirmPassword: undefined,
   });
   jwt.sign({ id: admin._id }, process.env.JWT_SECRET);
   return admin;

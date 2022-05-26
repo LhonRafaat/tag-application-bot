8de9113a11aaa19580a96f618b4416ba956f92d4
lhonRafaat <lhontaekwondo@gmail.com>
2022-05-26 23:30:23 +0300

    added update user route

--- routes/memberRoutes.js
+++ routes/memberRoutes.js
@@ -1,9 +1,10 @@
 import express from "express";
 import { deleteMember, getMembers } from "../controllers/membersController.js";
+import { updateUser } from "../services/memberService.js";
 
 const router = express.Router();
 
-router.route("/api/members").get(getMembers);
+router.route("/api/members").get(getMembers).patch(updateUser);
 router.route("/api/members/:id").delete(deleteMember);
 
 export default router;

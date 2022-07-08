83c12ac9421321794e2f83d6846f36006735d21c
lhonRafaat <lhontaekwondo@gmail.com>
2022-07-09 00:47:26 +0300

    added check for roles when registering bf2 account

--- modal.js
+++ modal.js
@@ -219,7 +219,7 @@ export const getModal = (client) => {
       //TODO: send a error message when user doesnt exist
 
       // big problem here, if ephemeral is true, we cannot react to the messag
-      return await await interaction.editReply({
+      return await interaction.editReply({
         ephemeral: true,
         files: [attachment],
       });
@@ -554,6 +554,73 @@ export const getModal = (client) => {
         bf2NameVal,
         modal.user.username
       );
+
+      if (
+        (await modal.member.roles.cache.some((role) => {
+          return [
+            settings[0].idfXboxId,
+            settings[0].idfPcId,
+            settings[0].idfPsId,
+          ].includes(role.id);
+        })) &&
+        !(await modal.member.roles.cache.some((role) => {
+          return [
+            settings[0].moderatorId,
+            settings[0].seniorModeratorId,
+            settings[0].trialModeratorId,
+            settings[0].designId,
+            settings[0].forceCodeId,
+            settings[0].adminId,
+            settings[0].modId,
+            settings[0].founderId,
+            settings[0].headAdminId,
+          ].includes(role.id);
+        }))
+      ) {
+        // idf registered tag
+        await modal.member.roles
+          .add(settings[0].registeredMember)
+          .catch((err) => {
+            console.log("Error" + err);
+          });
+      } else if (
+        //staff
+        await modal.member.roles.cache.some((role) => {
+          return [
+            settings[0].moderatorId,
+            settings[0].seniorModeratorId,
+            settings[0].trialModeratorId,
+            settings[0].designId,
+            settings[0].forceCodeId,
+            settings[0].adminId,
+          ].includes(role.id);
+        })
+      ) {
+        await modal.member.roles
+          .add(settings[0].registeredStaff)
+          .catch((err) => {
+            console.log("Error" + err);
+          });
+      } else if (
+        await modal.member.roles.cache.some((role) => {
+          return [
+            settings[0].modId,
+            settings[0].founderId,
+            settings[0].headAdminId,
+          ].includes(role.id);
+        })
+      ) {
+        // admins
+        await modal.member.roles
+          .add(settings[0].registeredMangment)
+          .catch((err) => {
+            console.log("Error" + err);
+          });
+      } else {
+        await modal.member.roles.add(settings[0].candidateId).catch((err) => {
+          console.log("Error" + err);
+        });
+      }
       await modal.deferReply({ ephemeral: true });
       return await modal.followUp({
         content: "successfully registered",

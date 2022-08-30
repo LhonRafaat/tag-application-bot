daa3d913acdb1e83bac9597283de74fa0fbdef27
lhonRafaat <lhontaekwondo@gmail.com>
2022-08-30 23:48:30 +0300

    small changes

--- utils/hasReachedVotes.js
+++ utils/hasReachedVotes.js
@@ -44,7 +44,7 @@ export const hasReachedVotes = async (member, settings, client) => {
           },
           {
             id: mods.id,
-            allow: ["VIEW_CHANNEL", "ADMINISTRATOR"],
+            allow: ["VIEW_CHANNEL"],
           },
         ],
       });

a42fce72cdc1804297fbbe88f66f48f68bae07ed
lhonRafaat <lhontaekwondo@gmail.com>
2022-10-01 15:05:53 +0300

    add user points, made the canvas responsive, and sorted data

--- UI/tournamentPlate.js
+++ UI/tournamentPlate.js
@@ -7,6 +7,7 @@ export const getTournamentPlate = async () => {
       name: "John Doe",
       points: 100,
     },
+
     {
       name: "Jane Doe",
       points: 200,
@@ -14,6 +15,30 @@ export const getTournamentPlate = async () => {
     {
       name: "John Don",
       points: 300,
+    },
+    {
+      name: "Jane Don",
+      points: 400,
+    },
+    {
+      name: "Jane Don",
+      points: 400,
+    },
+    {
+      name: "Jane Don",
+      points: 400,
+    },
+    {
+      name: "Jane Don",
+      points: 400,
+    },
+    {
+      name: "Jane Don",
+      points: 400,
+    },
+    {
+      name: "Jane Don",
+      points: 400,
     },
     {
       name: "Jane Don",
@@ -27,7 +52,7 @@ export const getTournamentPlate = async () => {
     family: "share-bold",
   });
 
-  const canvas = Canvas.createCanvas(5000, 2000);
+  const canvas = Canvas.createCanvas(5000, 2000 + sampleUsers.length * 100);
   const context = canvas.getContext("2d");
   // order is acting like z-index here
   const background = await Canvas.loadImage("./assets/images/bf-bg.png");
@@ -52,23 +77,35 @@ export const getTournamentPlate = async () => {
   context.strokeStyle = "white";
   context.stroke();
   //user row
-  sampleUsers.forEach((user, index) => {
-    let y = 500;
-    for (let i = 0; i < index; i++) {
-      y += 210;
-    }
-    context.globalAlpha = 0.2;
-    context.fillStyle = "gray";
-    context.fillRect(200, y, canvas.width - 400, 200);
-    context.globalAlpha = 1;
-    context.fillStyle = "gray";
-    context.fillText(user.name, 350, y + 120);
-    context.fillText(index + 1, 240, y + 120);
-    context.fillStyle = "red";
-    context.globalAlpha = 0.2;
-    context.fillRect(210, y + 45, 100, 100);
-    context.globalAlpha = 1;
-  });
+  sampleUsers
+    .sort((a, b) => {
+      if (a.points > b.points) {
+        return -1;
+      }
+      if (a.points < b.points) {
+        return 1;
+      }
+      return 0; 
+    })
+    .forEach((user, index) => {
+      let y = 500;
+      for (let i = 0; i < index; i++) {
+        y += 210;
+      }
+      context.globalAlpha = 0.2;
+      context.fillStyle = "gray";
+      context.fillRect(200, y, canvas.width - 400, 200);
+      context.globalAlpha = 1;
+      context.fillStyle = "gray";
+      context.fillText(user.name, 350, y + 120);
+      context.fillText(index + 1, 240, y + 120);
+      context.fillStyle = "white";
+      context.fillText(user.points, canvas.width - 350, y + 120);
+      context.fillStyle = "red";
+      context.globalAlpha = 0.2;
+      context.fillRect(210, y + 45, 100, 100);
+      context.globalAlpha = 1;
+    });
 
   // Use the helpful Attachment class structure to process the file for you
   const attachment = new MessageAttachment(

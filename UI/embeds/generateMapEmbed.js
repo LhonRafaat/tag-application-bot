3edd2e47c4cbb3ed9020ac08a852500e679c0eaa
lhonRafaat <lhontaekwondo@gmail.com>
2024-06-17 01:11:46 +0300

    added back idf logo

--- UI/embeds/generateMapEmbed.js
+++ UI/embeds/generateMapEmbed.js
@@ -17,9 +17,9 @@ export const generateMapEmbed = (
       `- **Server Id**: ${serverId}\n- **Current Players**: ${currentPlayers}\n- **Current Map**: ${currentMap}\n- **Region**: ${region}`
     )
     .setImage(image)
-    // .setThumbnail(
-    //   "https://eaassets-a.akamaihd.net/battlelog/prod/emblem/392/590/320/2955055690685760912.png?v=1537200736"
-    // )
+    .setThumbnail(
+      "https://eaassets-a.akamaihd.net/battlelog/prod/emblem/392/590/320/2955055690685760912.png?v=1537200736"
+    )
     .setColor("#f500d4");
 
   return embed;

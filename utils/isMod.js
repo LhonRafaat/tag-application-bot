7bf855b2224fa5fa1e4652baf6ef3d0dfeb174b1
lhonRafaat <lhontaekwondo@gmail.com>
2024-06-19 16:52:06 +0300

    changed find to some to isMod

--- utils/isMod.js
+++ utils/isMod.js
@@ -1,5 +1,5 @@
 export const isMod = async (interaction, settings) => {
-  const isAuthorized = await interaction.member.roles.cache.find((role) => {
+  const isAuthorized = await interaction.member.roles.cache.some((role) => {
     return [
       settings[0].founderId,
       settings[0].headAdminId,

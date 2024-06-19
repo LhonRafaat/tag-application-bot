daf4888ff58bc697bb923d3f8f828aab75120b86
lhonRafaat <lhontaekwondo@gmail.com>
2024-06-19 17:11:49 +0300

    fixed setpoints

--- interactions/setPoints.js
+++ interactions/setPoints.js
@@ -1,9 +1,9 @@
 import { setRequiredPoints } from "../services/settingService.js";
 import { isMod } from "../utils/isMod.js";
 
-export const setPoints = async (interaction) => {
+export const setPoints = async (interaction, settings) => {
   // check user if is head admin or founder
-  const isAuthorized = await isMod(interaction, setPoints);
+  const isAuthorized = await isMod(interaction, settings);
   if (!isAuthorized) {
     return await interaction.editReply({
       content: "You are not authorized",
@@ -12,11 +12,6 @@ export const setPoints = async (interaction) => {
   }
   // here we should check that only admins could do that
   const points = await interaction.options.getNumber("points");
-  setRequiredPoints(points);
-  return points
-    ? await interaction.editReply({ content: "okay", ephemeral: true })
-    : await interaction.editReply({
-        content: "bad request",
-        ephemeral: true,
-      });
+  await setRequiredPoints(points);
+  return await interaction.editReply({ content: "okay", ephemeral: true });
 };

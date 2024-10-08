3a39f789b758e727652beac31689a96e39d4d724
lhonRafaat <lhontaekwondo@gmail.com>
2024-10-08 23:59:48 +0300

    update thumbnail

--- UI/embeds/generateMapEmbed.js
+++ UI/embeds/generateMapEmbed.js
@@ -22,7 +22,7 @@ export const generateMapEmbed = (
     )
     .setImage(image)
     .setThumbnail(
-      "https://i.namu.wiki/i/WWaZjrK6XJkO8IYRQSA64DSX-EISMeKLjuSMu9eXijktor1SYAqbRLX3xI-uQ4CI6v8VLkblNWYnIlwx77ngQg.webp"
+      "https://cdn.discordapp.com/attachments/967381487924150322/1293316522277601462/DA7795C0-9BB8-4D7F-9FB9-3EF28D032BEB.jpg?ex=6706ee5f&is=67059cdf&hm=fc10535d8d36f28cac0613087a55fe0bb7617fe8cb97133767d2b34f03c5ef18&"
     )
     .setColor("#f500d4");
 

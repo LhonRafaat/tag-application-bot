c2d3668e854b0274a5e6a953074517f8e03e35de
lhonRafaat <lhontaekwondo@gmail.com>
2024-10-16 23:03:52 +0300

    small adjustments

--- modal-submit/SubLinkAlt.js
+++ modal-submit/SubLinkAlt.js
@@ -2,9 +2,9 @@ import { findAll, findOne } from "../services/memberService.js";
 
 export const subLinkAlt = async (interaction, returnedMember, platformVal) => {
   // we check if this account is linked by someone else already
-  let members = await findAll();
-  let originIds = [];
-  members.map((member) => originIds.push(...member.originIds));
+  const members = await findAll();
+  const originIds = [];
+  members.forEach((member) => originIds.push(...member.originIds));
   if (originIds.includes(returnedMember.data?.id)) {
     return await interaction.editReply({
       content: "This account is already linked",

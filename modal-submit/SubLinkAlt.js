fad0c31622bee888492ed94b1fd2db10b149176c
lhonRafaat <lhontaekwondo@gmail.com>
2024-10-16 23:15:54 +0300

    add to string to returned user id

--- modal-submit/SubLinkAlt.js
+++ modal-submit/SubLinkAlt.js
@@ -5,7 +5,7 @@ export const subLinkAlt = async (interaction, returnedMember, platformVal) => {
   const members = await findAll();
   const originIds = [];
   members.forEach((member) => originIds.push(...member.originIds));
-  if (originIds.includes(returnedMember.data?.id)) {
+  if (originIds.includes(returnedMember.data?.id?.toString())) {
     return await interaction.editReply({
       content: "This account is already linked",
       ephemeral: true,

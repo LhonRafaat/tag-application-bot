e77466c562a07f7615540a980e2f06bc4e10c33d
lhonRafaat <lhontaekwondo@gmail.com>
2024-10-16 23:21:15 +0300

    add isAccountLinked helper

---
+++ utils/isAccountAlreadyLinked.js
@@ -0,0 +1,13 @@
+import { findAll } from "../services/memberService.js";
+
+export const isAccountAlreadyLinked = async (returnedMember, interaction) => {
+  // we check if this account is linked by someone else already
+  const members = await findAll();
+  const originIds = [];
+  members.forEach((member) => originIds.push(...member.originIds));
+  if (originIds.includes(returnedMember.data?.id?.toString())) {
+    return await interaction.editReply({
+      content: "This account is already linked",
+    });
+  }
+};

052c8f5e9ff355d5d68345f9d1757a9b04833c67
lhonRafaat <lhontaekwondo@gmail.com>
2024-10-28 18:18:31 +0300

    update df ping msg

--- client.js
+++ client.js
@@ -328,7 +328,7 @@ export const client = async () => {
         // console.log(user);
 
         const botMsg = await msg.reply(
-          `Please only react if you going to participate in the dogfight \n - ${user.userNames[0]} \n`
+          `Please only react if you going to participate in the dogfight, after 2 hours from this ping, you cannot react. \n - ${user.userNames[0]} \n`
         );
         await botMsg.react(YES_EMOJI);
       }

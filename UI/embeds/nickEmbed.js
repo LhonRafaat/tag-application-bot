5cb6dc6a38956cc3fd69b3617ee64cd9b8668109
lhonRafaat <lhontaekwondo@gmail.com>
2025-02-28 18:48:50 +0300

    feat: fix embeds spacing

--- UI/embeds/nickEmbed.js
+++ UI/embeds/nickEmbed.js
@@ -4,7 +4,7 @@ export function nickEmbed() {
   const embed = new EmbedBuilder()
     .setTitle("Nickname not matching")
     .setDescription(
-      `Please match your Discord nickname with your ingame nickname. \n**How to** \nEdit your nickname by rightclicking on your name within this chat. It is a local setting on our server and wont affect your global nickname. \n`
+      `Please match your Discord nickname with your ingame nickname.\n\n**How to** \nEdit your nickname by rightclicking on your name within this chat. It is a local setting on our server and wont affect your global nickname. \n`
     );
 
   return embed;
--- UI/embeds/questionsEmbed.js
+++ UI/embeds/questionsEmbed.js
@@ -4,16 +4,7 @@ export function getQuestionsEmbed() {
   const applicationsEmbed = new EmbedBuilder()
     .setTitle("iDF Community")
     .setDescription(
-      `
-        Hi!\n
-       Before we can let you into our iDF Discord, we want to ask you a few simple questions!\n
-         1. **Why do you want to join us ?**\n
-            2. **Where did you hear of us?**\n
-            3. **Do you know somebody inside our community?**\n
-            4. **How would you describe yourself and your playstyle?**\n
-            5- **Please send a screenshot of your EA-App/PSN/XBox nickname, this is required to confirm your identity**.\n
-        
-        `
+      `Hi and welcome!\n\nBefore we can let you into our iDF Discord, we want to ask you a few simple questions!\n**1. Why do you want to join us ?**\n**2. Where did you hear of us?**\n**3. Do you know somebody inside our community?**\n**4. How would you describe yourself and your playstyle?**\n**5. Please send a screenshot of your EA-App/PSN/XBox nickname, this is required to confirm your identity**\n`
     );
 
   return applicationsEmbed;
